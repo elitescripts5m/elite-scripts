fecc08c64142a6c73728a12e7230e0e8c38d31c8
LifeGoal <35453587+LifeGoal@users.noreply.github.com>
2024-08-08 15:35:02 +0200

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+![logo_banner](logo_banner.jpg)
+
+# Elite Scripts
+The developers at Elite Scripts have been involved with FiveM since the FiveReborn era. We are a group of Swedes who have owned and developed some of the largest and best servers in Sweden. Our team comprises well-known developers proficient in LUA, Javascript, and other languages. Almost all of our resources are crafted in LUA, enabling our customers to edit them at their convenience.
+
+## Tebex
+https://elitescripts.tebex.io/
+
+### Help us get better
+We need help with translating our resources. Feel free to create your own translations file and make a pull request. It would mean the world to us!
---
+++ locales/sv.lua
@@ -0,0 +1,20 @@
+if not Config["lang"] == "sv" then return end
+
+Strings = {
+    -- Client locales
+    spot_blip_label = "Spot",
+    pickup_item = "Ta upp föremål",
+    picking_up_item = "Tar upp föremål..",
+    no_air = "Din tub är slut. Syret tar slut. Du måste skynda dig upp till ytan eller fylla på tuben snabbt.",
+    soon_no_air = "Din tank börjar ta slut. Fyll på den! Syrenivå: %s%",
+    put_on_gear_error = "Du måste stå på land för att sätta på dig dykarutrustningen.",
+    remove_gear = "Tar av dykarutrustning..",
+    removed_gear = "Du tog av dig dykarutrustningen.",
+    refilling_air = "Fyller lufttub..",
+    opening_treasure = "Öppnar skattkista..",
+
+    -- Server locales
+    full_inventory = "Du kan tyvärr inte bära mer. Lägg av dig lite grejer och kom tillbaka!",
+    found_nothing = "Fasiken också.. Här var det ju bara sand..",
+    empty_gear = "Denna dykarutrusning verkar vara tom.."
+}
