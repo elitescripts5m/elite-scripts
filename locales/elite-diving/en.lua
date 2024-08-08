416163a6508399ca84e5d05eebc3e9c07a6b0523
LifeGoal <35453587+LifeGoal@users.noreply.github.com>
2024-08-08 15:40:14 +0200

    Create en.lua

---
+++ locales/elite-diving/en.lua
@@ -0,0 +1,20 @@
+if not Config["lang"] == "en" then return end
+
+Strings = {
+    -- Client locales
+    spot_blip_label = "Spot",
+    pickup_item = "Pickup item",
+    picking_up_item = "Picking up item..",
+    no_air = "Your oxygen tube is running low. You must rush to the surface or fill the tube quickly.",
+    soon_no_air = "Your tube is soon empty. Fill it up! Oxygen level: %s%",
+    put_on_gear_error = "You must stand on land to put on the diving equipment.",
+    remove_gear = "Taking off scuba gear..",
+    removed_gear = "You took off your scuba gear.",
+    refilling_air = "Filling oxygen tube..",
+    opening_treasure = "Opening treasure chest..",
+
+    -- Server locales
+    full_inventory = "Unfortunately, you cannot carry more. Put some stuff down and come back!",
+    found_nothing = "Oh god no.. It was just sand here..",
+    empty_gear = "This scuba gear appears to be empty.."
+}
